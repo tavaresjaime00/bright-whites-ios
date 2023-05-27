b5ae048cb915fd2ca2a367621ec1b94658f59c72
Jaime Tavares <tavaresjaime00@gmail.com>
2023-05-27 15:59:56 -0400

    Initial Commit

---
+++ bright-whites/bright_whitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  bright_whitesApp.swift
+//  bright-whites
+//
+//  Created by Jaime Tavares on 2023-05-27.
+//
+
+import SwiftUI
+
+@main
+struct bright_whitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
