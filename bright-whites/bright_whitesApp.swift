0abd9c3953219aa02bd43142f54a04bc41b7855e
Jaime Tavares <tavaresjaime00@gmail.com>
2023-05-28 01:19:52 -0400

    Created tooth-brushing app.

--- bright-whites/bright_whitesApp.swift
+++ bright-whites/bright_whitesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct bright_whitesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().background(.black)
         }
     }
 }
